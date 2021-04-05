d507bfd4a07ea5c09775d59b013e53e52ca8214c
root <root@ns3317563.ip-5-135-165.eu>
2021-04-05 13:12:03 +0200

    corrections

--- backend/api/comments/controllers/comments.js
+++ backend/api/comments/controllers/comments.js
@@ -5,4 +5,50 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  // this method is called when api to create comment is called
+    async create(ctx) {
+        // add user from the request and add it to the body of request
+        ctx.request.body.user = ctx.state.user.id;
+        // call the function to creating comment with data
+        let entity = await strapi.services.comments.create(ctx.request.body);
+        // return data for api after removing field which are not exported
+        return sanitizeEntity(entity, { model: strapi.models.comments });
+    },
+    async update(ctx) {
+        // get the id of comment which is updated
+        const { id } = ctx.params;
+        // finding the comment for user and id
+        const [comment] = await strapi.services.comments.find({
+            id: ctx.params.id,
+            'user.id': ctx.state.user.id,
+        });
+        // comment does not exist send error
+        if (!comment) {
+            return ctx.unauthorized(`You can't update this entry`);
+        }
+        // update the comment
+        let entity = await strapi.services.comments.update({ id }, ctx.request.body);
+         // return data for api after removing field which are not exported
+        return sanitizeEntity(entity, { model: strapi.models.comments });
+    },
+    async delete(ctx) {
+        // get the id of comment which is updated
+        const { id } = ctx.params;
+        // finding the comment for user and id
+        const [comment] = await strapi.services.comments.find({
+            id: ctx.params.id,
+            'user.id': ctx.state.user.id,
+        });
+        // comment does not exist send error
+        if (!comment) {
+            return ctx.unauthorized(`You can't update this entry`);
+        }
+        // delete the comment
+        let entity = await strapi.services.comments.delete({ id });
+         // return data for api after removing field which are not exported
+        return sanitizeEntity(entity, { model: strapi.models.comments });
+    },
+};
