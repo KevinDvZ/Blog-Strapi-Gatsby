c480e91f3cb25a1b2e5ae0c99916704442ccd998
DEVEZA <kevin.deveza@gmail.com>
2021-04-10 17:44:42 +0200

    fetch correction final ??????

--- frontend/src/components/comments.js
+++ frontend/src/components/comments.js
@@ -1,7 +1,7 @@
 
 import '../assets/css/comment.css';
 import React from 'react'
-
+const fetch = require("node-fetch");
 
 let comments = [];
 const apiUrl = process.env.API_URL;
@@ -33,7 +33,7 @@ const submitForm = ((ev, strapiId) => {
 
 
 const Comments = ({ article }) => {
-    const fetch = require("node-fetch");
+
     fetch(
          apiUrl + "/comments?article=" +
         article.strapiId
